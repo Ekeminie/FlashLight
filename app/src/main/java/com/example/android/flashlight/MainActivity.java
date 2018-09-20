15ecb49f930f407142e0db6991f286ae196bdce9
Goodnew <mikeblize@gmail.com>
2018-09-20 18:13:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/flashlight/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.android.flashlight;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageFlashlight;
+    private static final int CAMERA_REQUEST = 50;
+    private boolean flashLightStatus = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageFlashlight = findViewById(R.id.switchBtn);
+
+        final boolean hasCameraFlash = getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        boolean isEnabled = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+
+        imageFlashlight.setEnabled(isEnabled);
+
+//        buttonEnable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST);
+//            }
+//        });
+
+
+        imageFlashlight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hasCameraFlash){
+                    if (flashLightStatus){
+                        flashLightOff();
+                    }else {
+                        flashLightOn();
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, "No Flash Available for this Device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void flashLightOn() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+            flashLightStatus = true;
+            imageFlashlight.setImageResource(R.drawable.lighton);
+        } catch (CameraAccessException e) {
+        }
+
+    }
+
+    private void flashLightOff() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+            flashLightStatus = false;
+            imageFlashlight.setImageResource(R.drawable.lightoff);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case CAMERA_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                    buttonEnable.setEnabled(false);
+//                    buttonEnable.setText("Camera Enabled");
+                    imageFlashlight.setEnabled(true);
+
+
+                }else{
+                    Toast.makeText(MainActivity.this, "FlashLight Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
